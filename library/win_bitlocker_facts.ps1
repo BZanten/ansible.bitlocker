4f47dd31863490de00013c382d7907ce2b129d12
Ben van Zanten <Ben@van.Zanten.name>
2021-08-06 23:59:59 +0200

    Migrated scripts to newer version of Ansible PowerShell module

--- library/win_bitlocker_facts.ps1
+++ library/win_bitlocker_facts.ps1
@@ -5,21 +5,23 @@
 # Copyright: (c) 2020, Evi Vanoost <evi.vanoost@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-#Requires -Module Ansible.ModuleUtils.ArgvParser
-#Requires -Module Ansible.ModuleUtils.CommandUtil
-#Requires -Module Ansible.ModuleUtils.Legacy
-
+#AnsibleRequires -CSharpUtil Ansible.Basic
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 2.0
 
-# Create a new result object
-$result = @{
-    changed       = $false
-    ansible_facts = @{
-        ansible_bitlocker = @()
-    }
+# This modules does not accept any options
+$spec = @{
+    supports_check_mode = $true
 }
 
-$result.ansible_facts.ansible_bitlocker = @(Get-BitlockerVolume)
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
 
-Exit-Json -obj $result
+$module.LogEvent("Get-BitLockerVolume",'Information',$True)
+
+# Return information
+$module.Result.changed = $False
+$module.Result.ansible_facts = @{
+    ansible_bitlocker = @( Get-BitLockerVolume )
+}
+
+$module.ExitJson()
--- library/win_tpm_facts.ps1
+++ library/win_tpm_facts.ps1
@@ -4,22 +4,23 @@
 # Copyright: (c) 2019, ITIGO AG <opensource@itigo.ch>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-#Requires -Module Ansible.ModuleUtils.ArgvParser
-#Requires -Module Ansible.ModuleUtils.CommandUtil
-#Requires -Module Ansible.ModuleUtils.Legacy
-
+#AnsibleRequires -CSharpUtil Ansible.Basic
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 2.0
 
-# Create a new result object
-$result = @{
-    changed       = $false
-    ansible_facts = @{
-        ansible_tpm = @{}
-    }
+# This modules does not accept any options
+$spec = @{
+    supports_check_mode = $true
 }
 
-$result.ansible_facts.ansible_tpm = Get-Tpm
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
 
-# Return result
-Exit-Json -obj $result
+$module.LogEvent("Get-Tpm",'Information',$True)
+
+# Return information
+$module.Result.changed = $False
+$module.Result.ansible_facts = @{
+    ansible_tpm = Get-Tpm
+}
+
+$module.ExitJson()
