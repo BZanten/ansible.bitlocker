9ba1759072976e4bd66475d05f9363d8bd12079f
Ben van Zanten <Ben@van.Zanten.name>
2021-08-07 00:00:54 +0200

    Added new parameters to the module, migrated module to newer version

--- library/win_bitlocker.ps1
+++ library/win_bitlocker.ps1
@@ -4,36 +4,50 @@
 # Copyright: (c) 2019, ITIGO AG <opensource@itigo.ch>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-#Requires -Module Ansible.ModuleUtils.ArgvParser
-#Requires -Module Ansible.ModuleUtils.CommandUtil
-#Requires -Module Ansible.ModuleUtils.Legacy
-
+#AnsibleRequires -CSharpUtil Ansible.Basic
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 2.0
 
-$params     = Parse-Args -arguments $args -supports_check_mode $true
-$check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
-$diff       = Get-AnsibleParam -obj $params -name "_ansible_diff"       -type "bool" -default $false
+$spec = @{
+    options = @{
+        mount        = @{ type = "str"; required = $true }
+        state        = @{ type = "str"; required = $false ; default='enabled'; choices= 'enabled','disabled' }
+        encryption   = @{ type = "str"; required = $false; choices = 'Aes128','Aes256','XtsAes128','XtsAes256' }
+        keyprotector = @{
+            type = 'str'
+            default = 'RecoveryPasswordProtector'
+            choices = 'RecoveryPasswordProtector', 'TpmProtector'
+        }
+        backup_to_ad        = @{ type = "bool" }
+        hardware_encryption = @{ type = "bool" }
+        skip_hardware_test  = @{ type = "bool" }
+        used_space_only     = @{ type = "bool" }
+    }
+    supports_check_mode = $true
+}
 
-# of the following Parametersets: -AdAccountOrGroupProtector, -PasswordProtector, -TpmAndPinProtector, -TpmAndPinAndStartupKeyProtector, -RecoveryKeyProtector, -RecoveryPasswordProtector, -StartupKeyProtector, -TpmAndStartupKeyProtector, -TpmProtector
-#    only the following 2 are currently implemented: -RecoveryPasswordProtector and -TpmProtector.
-# Note the Enable-BitLocker cmdlet only allows one of these methods or combinations when you enable encryption, but you can use the Add-BitLockerKeyProtector cmdlet to add other protectors.
-$mount        = Get-AnsibleParam -obj $params -name "mount"        -type "str" -failifempty $true
-$state        = Get-AnsibleParam -obj $params -name "state"        -type "str" -default "enabled" -validateset "enabled", "disabled"
-$encryption   = Get-AnsibleParam -obj $params -name "encryption"   -type "str" -validateset "Aes128","Aes256","XtsAes128","XtsAes256"
-$keyprotector = Get-AnsibleParam -obj $params -name "keyprotector" -type "str" -default "RecoveryPasswordProtector" -validateset "RecoveryPasswordProtector","TpmProtector"
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
+
+$mount        = $module.Params.mount
+$state        = $module.Params.state
+$keyprotector = $module.Params.keyprotector
+$backup2AD    = $module.Params.backup_to_ad
+# Create hashtables for parameter splatting of several other optional parameters, later
+if ([string]::IsNullOrEmpty($module.Params.encryption)) { $encryptionParam=@{} } else { $encryptionParam=@{'EncryptionMethod'=$module.Params.encryption} }
+if ([Boolean]$module.Params.hardware_encryption) { $hardwareEncrParam=@{}     } else { $hardwareEncrParam=@{'HardwareEncryption'=$True} }
+if ([Boolean]$module.Params.skip_hardware_test)  { $skipHardwareTestParam=@{} } else { $skipHardwareTestParam=@{'SkipHardwareTest'=$True} }
+if ([Boolean]$module.Params.used_space_only)     { $usedSpaceOnlyParam=@{}    } else { $usedSpaceOnlyParam=@{'UsedSpaceOnly'=$True} }
 
 # Create a new result object
-$result = @{
+$ret = @{
     changed = $false
+    after = $Null
+    before = $Null
+    value = @()
 }
 
-# Create a hashtable for parameter splatting of 'Encryption' later.
-if ([string]::IsNullOrEmpty($encryption)) {
-    $encryptionParam=@{}
-} else {
-    $encryptionParam=@{'EncryptionMethod'=$encryption}
-}
+# Start Time of the script, will be used to search the Windows Event logs for BitLocker events after this time.
+$StartScriptTime = Get-Date
 
 # BitLockering multiple drives; a typical configuration is where the C: drive is protected by TPM (TPM can only protect the OS volume),
 #   then the other volumes (data) are protected with information stored on the C: volume itself.
@@ -48,8 +62,6 @@ switch -Regex ($mount.ToString().Trim()) {
     default     { $runOnVolumes = $Null ; Write-Error "$_ is not found, no action"}
 }
 
-Add-Warning -obj $result -message "mount: $mount  ; "
-
 # The Enable-BitLockerAutoUnlock cmdlet enables automatic unlocking for a volume protected by BitLocker Disk Encryption.
 # You can configure BitLocker to automatically unlock volumes that do not host an operating system.
 #   After a user unlocks the operating system volume, BitLocker uses encrypted information stored in the registry and volume
@@ -59,28 +71,72 @@ if ($state -eq "enabled") {
     foreach ($runVolume in $runOnVolumes) {
         $protectionstatus = (Get-BitLockerVolume -MountPoint $runVolume).ProtectionStatus
         if ( $protectionstatus -eq "Off" ) {
-            if (-not $check_mode) {
+            if (-not $module.CheckMode) {
                 # Assume -TpmProtector  is used... But replace it with another one (RecoveryPasswordProtector) if NOT BitLockering the SystemDrive or if the arguments specify so.
                 $ProtectorParam = @{"TpmProtector"=$True}
                 if ( ($keyprotector -eq "RecoveryPasswordProtector") -or ($runVolume -ne $Env:SystemDrive) ) {
                     $ProtectorParam = @{"RecoveryPasswordProtector"=$True}
                 }
-                $res = Enable-BitLocker -MountPoint $runVolume @ProtectorParam @encryptionParam
-                $result.res = $res
+                try {
+                    $res = Enable-BitLocker -MountPoint $runVolume @ProtectorParam @encryptionParam @hardwareEncrParam @skipHardwareTestParam @usedSpaceOnlyParam -ErrorAction Stop
+                    $ret.value += $res
+                } catch {
+                    $module.FailJSON("Error enabling BitLocker", $_)
+                }
                 # if NOT BitLockering the system volume, add autounlock to the datadrives
                 if ($runVolume -ne $Env:SystemDrive) {
-                    $res = Enable-BitLockerAutoUnlock -MountPoint $runVolume
-                    $result.res += $res
-                }
-
-                # Backup the BitLocker Recovery key to AD... (if configured. Todo: parameterize this)
-                $RecoveryKeyProtector = Get-BitLockerVolume -MountPoint $runVolume | Select-Object -ExpandProperty KeyProtector | Where-Object { $_.KeyProtectorType -eq 'RecoveryPassword' }
-                if ($RecoveryKeyProtector) {
-                    $res = Backup-BitLockerKeyProtector -MountPoint $runVolume -KeyProtectorId $RecoveryKeyProtector.KeyProtectorId
-                    $result.res += $res
+                    try {
+                        $res = Enable-BitLockerAutoUnlock -MountPoint $runVolume -ErrorAction Stop
+                        $ret.value += $res
+                    } catch {
+                        $module.FailJSON("Error enabling BitLocker Autounlock for non-system volumes", $_)
+                    }
                 }
             }
-            $result.changed = $true
+            $ret.changed = $true
+        }
+
+        if ($backup2AD) {
+
+            # Always backup to (A)AD, even if the old protectionstatus was already On
+            # Backup the BitLocker Recovery key to (A)AD... (if configured)
+            # DomainRole: 0 = workgroup workstation, 1 = domain joined workstation, 2 = workgroup server, 3 = domain joined server, 4 = (Backup) Domain Controller, 5 = (Primary) Domain Controller
+            $DomainRole = (Get-CimInstance -ClassName win32_ComputerSystem).DomainRole
+            $RecoveryKeyProtector = Get-BitLockerVolume -MountPoint $runVolume | Select-Object -ExpandProperty KeyProtector | Where-Object { $_.KeyProtectorType -eq 'RecoveryPassword' }
+            if ($RecoveryKeyProtector) {    # if AD Domain joined...
+                if ($DomainRole -in (1,3,4,5)) {
+                    try {
+                        $res = Backup-BitLockerKeyProtector -MountPoint $runVolume -KeyProtectorId $RecoveryKeyProtector.KeyProtectorId -ErrorAction Stop
+                        $ret.value += $res
+                    } catch {
+                        $module.Warn([string]::Format("Device is AD Joined, backup to Active Directory failed: {0}", $_.ToString() ))
+                    }
+                } else { # Non AD-joined, maybe AAD joined or -enrolled.
+                    if ((dsregcmd /status ) -match 'AzureAdJoined\s*\:\s*YES') {
+                        try {
+                            $res = BackupToAAD-BitLockerKeyProtector -MountPoint $runVolume -KeyProtectorId $RecoveryKeyProtector.KeyProtectorId -ErrorAction Stop
+                            $ret.value += $res
+                            # Retrieve from the Windows Eventlog whether backup was successful:
+                            Start-Sleep -Seconds 2
+                            $WinEvents = Get-WinEvent -FilterHashTable @{LogName='Microsoft-Windows-BitLocker/BitLocker Management';Id=845; StartTime=$StartScriptTime} -ErrorAction SilentlyContinue
+                            if ($WinEvents) { $module.LogEvent("$WinEvents.Message")}
+                            else {
+                                $WinEvents = Get-WinEvent -FilterHashTable @{LogName='Microsoft-Windows-BitLocker/BitLocker Management';Id=846; StartTime=$StartScriptTime} -ErrorAction SilentlyContinue
+                                if ($WinEvents) { $module.Warn("$WinEvents.Message")}
+                                else {
+                                    $module.warn("Error backing up... no Windows event found")
+                                }
+                            }
+                        } catch {
+                            $module.Warn([string]::Format("Device is Azure AD Joined, backup to Azure Active Directory failed: {0}", $_.ToString() ))
+                        }
+                    } else {
+                        $module.Warn("Device is not AD joined, nor Azure AD joined, cannot backup key to (A)AD")
+                    }
+                }
+            }
+        } else {
+            $module.Warn("NOT backing up BitLocker recovery information to (A)AD since bitlocker_backup_to_ad is False")
         }
     }
 }
@@ -89,13 +145,22 @@ if ($state -eq "disabled") {
     foreach ($runVolume in $runOnVolumes) {
         $protectionstatus = (Get-BitLockerVolume -MountPoint $runVolume).ProtectionStatus
         if ( $protectionstatus -eq "On" ) {
-            if (-not $check_mode) {
+            if (-not $module.CheckMode) {
                 Disable-BitLocker -MountPoint $mount
             }
-            $result.changed = $true
+            $ret.changed = $true
         }
     }
 }
 
-# Return result
-Exit-Json -obj $result
+# # Return result
+# Exit-Json -obj $result
+
+# Return information
+$module.Diff.before = @( 'C:\' )
+$module.Diff.after = @( 'C:' )
+
+$module.Result.changed = $ret.changed
+$module.Result.value = $ret.value
+
+$module.ExitJson()
